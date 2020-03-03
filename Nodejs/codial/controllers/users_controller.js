ad5db958c8f6748757c97fdcb25ee268e6248686
Lavanya Singh <singhlavanya94@gmail.com>
2020-03-04 03:05:09 +0530

    make manual auth

--- Nodejs/codial/controllers/users_controller.js
+++ Nodejs/codial/controllers/users_controller.js
@@ -1,7 +1,24 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req,res){
-    return res.end("<h1>Inside the Profile</h1>");
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err,user){
+            if(err){
+                console.log("Error in finding the user");
+                return;
+            }
+            if(user){
+                return res.render('user_profile',{
+                    title:"User Profile",
+                    user:user
+                });
+            }
+            return res.redirect('/users/sign-in');
+        });
+    }
+    else{
+        return res.redirect('/users/sign-in');        
+    }
 };
 
 
@@ -50,5 +67,31 @@ module.exports.create = function(req,res){
 
 //sign in and create the session for the user
 module.exports.createSession = function(req,res){
+    //find the user
+    User.findOne({
+        email:req.body.email
+    },function(err,user){
+        if(err){
+            console.log("error in finding the user");
+            return;
+        }
+        //handle user found
+        if(user){
+            //handle passwords do not match
+            if(user.password!=req.body.password){
+                return res.redirect('back');
+            }
+            //handle session creation
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+        }
+        else{
+            //handle user not found
+            return res.redirect('back');
+        }
+    });
+
+
+
 
 } 
