146ddad66aa67278f1d7d5bed5c1602b1d9accce
Lavanya Singh <singhlavanya94@gmail.com>
2020-03-07 17:44:54 +0530

    add double check authentiction on home page

--- Nodejs/codial/controllers/home_controller.js
+++ Nodejs/codial/controllers/home_controller.js
@@ -1,8 +1,22 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    console.log(req.cookies);
-    return res.render('home',{
-        title:"Profile"
-    });
-    // return res.end("<h1>In the home controller</h1>");
+    //console.log(req.cookies);
+
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title:"Codial||Home",
+    //         posts:posts
+    //     });    
+    // });
+
+
+    //populate the user of each post    
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Codial||Home",
+            posts:posts
+        });  
+    })
 }
 
