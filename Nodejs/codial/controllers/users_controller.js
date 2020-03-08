a9032b9ac053c4347a9ae4303f5c7d4d572e50cb
Lavanya Singh <singhlavanya94@gmail.com>
2020-03-08 16:04:47 +0530

    convert to async await

--- Nodejs/codial/controllers/users_controller.js
+++ Nodejs/codial/controllers/users_controller.js
@@ -70,3 +70,14 @@ module.exports.destroySession = function(req,res){
     req.logout();
     return res.redirect('/');
 }
+
+
+module.exports.update = function(req,res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, {name:req.body.name, email:req.body.email},function(err,user){
+            return res.redirect('back');
+        })
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
+}
