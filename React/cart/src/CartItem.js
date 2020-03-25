6d767cdf8629423a6e0e0751f23510045e7a078d
Lavanya Singh <singhlavanya94@gmail.com>
2020-03-25 19:14:29 +0530

    make react app cart

---
+++ React/cart/src/CartItem.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            price:999,
+            title:'Phone',
+            qty:1,
+            img:''
+        }
+    }
+    render(){
+        const {price, title, qty} = this.state;
+        return(
+            <div className = "cart-item">
+                <div className= "left-block">
+                    <img style = {styles.image} alt = ""/>
+                </div>
+                <div className = "right-block">
+                    <div style = {{fontSize:25}}> {title} </div>
+                    <div style = {{color:'#777'}}> {price} </div>
+                    <div style = {{color:'#777'}}>{qty}</div>
+                    <div className = "cart-item-actions">
+                        {/*Buttons*/}
+                        <img alt = "increase" className = "action-icons" src ="https://image.flaticon.com/icons/svg/2089/2089588.svg"/>
+                        <img alt = "decrease" className = "action-icons" src ="https://image.flaticon.com/icons/svg/1828/1828906.svg"/>
+                        <img alt = "delete" className = "action-icons" src ="https://image.flaticon.com/icons/svg/1214/1214428.svg"/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+const styles = {
+    image:{
+        height:100,
+        width:100,
+        borderRadius:4,
+        background:'#ccc'
+    }
+}
+
+export default CartItem;
