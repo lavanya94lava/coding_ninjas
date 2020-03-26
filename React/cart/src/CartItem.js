7508a908d64216c7f4f6999b6b7745e679fb944a
Lavanya Singh <singhlavanya94@gmail.com>
2020-03-26 19:02:53 +0530

    add setState react

--- React/cart/src/CartItem.js
+++ React/cart/src/CartItem.js
@@ -10,6 +10,19 @@ class CartItem extends React.Component{
             img:''
         }
     }
+    increaseQuantity = () =>{
+        console.log("this",this.state);
+        // this.setState({
+        //     qty:this.state.qty+1
+        // });
+
+        //if previous state is required
+        this.setState((preState) => {
+            return {
+                qty: preState.qty+1
+            }
+        });
+    }
     render(){
         const {price, title, qty} = this.state;
         return(
@@ -23,7 +36,11 @@ class CartItem extends React.Component{
                     <div style = {{color:'#777'}}>{qty}</div>
                     <div className = "cart-item-actions">
                         {/*Buttons*/}
-                        <img alt = "increase" className = "action-icons" src ="https://image.flaticon.com/icons/svg/2089/2089588.svg"/>
+                        <img 
+                        alt = "increase" 
+                        className = "action-icons"
+                        src ="https://image.flaticon.com/icons/svg/2089/2089588.svg"
+                        onClick= {this.increaseQuantity}/>
                         <img alt = "decrease" className = "action-icons" src ="https://image.flaticon.com/icons/svg/1828/1828906.svg"/>
                         <img alt = "delete" className = "action-icons" src ="https://image.flaticon.com/icons/svg/1214/1214428.svg"/>
                     </div>
